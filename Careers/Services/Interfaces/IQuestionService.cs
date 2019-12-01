8a210339de0816b26ca091f34d58703dde3333ae
Romil <romil.masimov@gmail.com>
2019-12-01 13:35:50 +0400

    Methods added to IQuestionService interface

--- Careers/Services/Interfaces/IQuestionService.cs
+++ Careers/Services/Interfaces/IQuestionService.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using Careers.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Careers.Services.Interfaces
 {
-    interface IQuestionService
+    public interface IQuestionService
     {
+        Task<Question> InsertAsync(Question question);
+        Task<Question> UpdateAsync(Question question);
+        Task<bool> DeleteAsync(Question question);
+        Task<IEnumerable<Question>> FindAllAsync(int subCategoryId);
     }
 }
