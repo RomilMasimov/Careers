46223344b557b06b7d219149c2decb065962ffd6
Romil <romil.masimov@gmail.com>
2019-12-01 14:05:26 +0400

    QuestionSercice implementation added

---
+++ Careers/Services/QuestionService.cs
@@ -0,0 +1,54 @@
+﻿using Careers.EF;
+using Careers.Models;
+using Careers.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Careers.Services
+{
+    public class QuestionService : IQuestionService
+    {
+        private readonly CareersDbContext context;
+
+        public QuestionService(CareersDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<bool> DeleteAsync(Question question)
+        {
+            context.Questions.Remove(question);
+            await context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<IEnumerable<Question>> FindAllAsync(int subCategoryId)
+        {
+            return await context.Questions
+                .Where(m => m.SubCategoryId == subCategoryId)
+                .Include(m => m.QuestionAnswers)
+                .Include(m => m.Answers)
+                .Include(m => m.FromAnswers)
+                .Include(m => m.DefaultQuestions)
+                .ToListAsync();
+        }
+
+        public async Task<Question> InsertAsync(Question question)
+        {
+            question.Id = 0;
+            var res = await context.Questions.AddAsync(question);
+            await context.SaveChangesAsync();
+            return res.Entity;
+        }
+
+        public async Task<Question> UpdateAsync(Question question)
+        {
+            var res = context.Questions.Update(question);
+            await context.SaveChangesAsync();
+            return res.Entity;
+        }
+    }
+}
