0a49b7e799786e9c6a0e34fff365f397ad1f5745
imranveliyev <killer-boy95@mail.ru>
2019-12-04 16:10:31 +0400

    mail sender ready.

--- Careers/Services/EmailService.cs
+++ Careers/Services/EmailService.cs
@@ -7,14 +7,12 @@ namespace Careers.Services
     {
         public void SendEmail(string email, string subject, string message)
         {
-
-            var mail = new MailMessage {Subject = subject, Body = message};
+            var mail = new MailMessage { Subject = subject, Body = message ,IsBodyHtml = true};
             mail.To.Add(email);
-            mail.From = new MailAddress("sayrus719.noreply@gmail.com");
+            mail.From = new MailAddress("sayrus719.noreply@gmail.com","I AM YOUR DADDY");
 
-            var smtpServer = new SmtpClient("smtp.gmail.com")
+            var smtpServer = new SmtpClient("smtp.gmail.com", 587)
             {
-                Port = 587,
                 Credentials = new NetworkCredential("sayrus719.noreply@gmail.com", "P@ssword123456"),
                 EnableSsl = true
             };
