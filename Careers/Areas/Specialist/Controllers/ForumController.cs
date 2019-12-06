1871ef10e9f4fc76de62f7a03568d9b023e89f01
Romil <romil.masimov@gmail.com>
2019-12-06 23:49:47 +0400

    Specialist area added

---
+++ Careers/Areas/Specialist/Controllers/ForumController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Careers.Areas.Specialist.Controllers
+{
+    public class ForumController : Controller
+    {
+    }
+}
