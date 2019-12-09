51297124833f1ce6174f5fd8f6639b6f03d479e4
imranveliyev <killer-boy95@mail.ru>
2019-12-09 17:31:45 +0400

    signOut added!

--- Careers/Controllers/AuthController.cs
+++ Careers/Controllers/AuthController.cs
@@ -2,20 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Careers.Models.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Careers.Controllers
 {
     public class AuthController : Controller
     {
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AuthController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
         public IActionResult SignIn()
         {
             return View();
         }
 
-        public IActionResult SignOut()
+        public async Task<IActionResult> SignOut()
         {
-            return View();
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction("Index","Home",new {area=""});
         }
 
         public IActionResult SignUp()
