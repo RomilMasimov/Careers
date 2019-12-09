bc30d3ee6dbcee4646fadd950fc789c088502570
imranveliyev <killer-boy95@mail.ru>
2019-12-09 11:24:33 +0400

    localization testing

--- Careers/Controllers/HomeController.cs
+++ Careers/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Careers.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
 
 namespace Careers.Controllers
 {
@@ -11,18 +12,20 @@ namespace Careers.Controllers
     {
         private readonly ICategoryService _categoryService;
         private readonly IReviewService _reviewService;
+        private readonly IStringLocalizer<SharedResource> _sharedLocalizer;
 
-        public HomeController(ICategoryService categoryService, IReviewService reviewService)
+        public HomeController(ICategoryService categoryService, IReviewService reviewService,IStringLocalizer<SharedResource> sharedLocalizer)
         {
             _categoryService = categoryService;
             _reviewService = reviewService;
+            _sharedLocalizer = sharedLocalizer;
         }
 
         public async Task<IActionResult> Index()
         {
             var categories = await _categoryService.GetAllCategories(true);
             var lastReviews = await _reviewService.GetLastReviewsAsync(4);
-
+            string localization = _sharedLocalizer["Hello"];
             return View();
         }
 
