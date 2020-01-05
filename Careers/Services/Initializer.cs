872bc817b19215b450fafb35c63d8392736a7758
RomilMasimov <46720172+RomilMasimov@users.noreply.github.com>
2020-01-05 13:57:47 +0400

    Initializer and frontend fix

--- Careers/Services/Initializer.cs
+++ Careers/Services/Initializer.cs
@@ -45,11 +45,11 @@ namespace Careers.Services
 
         public void Languages()
         {
-            if(context.Languages.Any()) return;
-            context.Languages.Add(new MyLanguage{Name = "azerbaijan"});
-            context.Languages.Add(new MyLanguage{Name = "russian"});
-            context.Languages.Add(new MyLanguage{Name = "english"});
-            context.Languages.Add(new MyLanguage{Name = "arabic" });
+            if (context.Languages.Any()) return;
+            context.Languages.Add(new MyLanguage { Name = "azerbaijan" });
+            context.Languages.Add(new MyLanguage { Name = "russian" });
+            context.Languages.Add(new MyLanguage { Name = "english" });
+            context.Languages.Add(new MyLanguage { Name = "arabic" });
             context.SaveChanges();
         }
 
@@ -131,46 +131,55 @@ namespace Careers.Services
 
         public void QuestionAndAnswers()
         {
-            var subcategory = context.SubCategories.FirstOrDefault(x => x.DescriptionRU.ToLower() == "программисты");
             if (context.Questions.Any()) return;
-            var q1 = context.Questions.Add(new Question { Type = QuestionTypeEnum.Input, TextRU = "Что необходимо разработать?", TextAZ = "neynemek lazimdir?", SubCategory = subcategory });
-            var q2 = context.Questions.Add(new Question { Type = QuestionTypeEnum.Input, TextRU = "Для чего?", TextAZ = "Ne ucun?", SubCategory = subcategory });
-            var q3 = context.Questions.Add(new Question { Type = QuestionTypeEnum.Single, TextRU = "Язык программирования?", TextAZ = "Hansi dilinde?", SubCategory = subcategory });
-            var q4 = context.Questions.Add(new Question { Type = QuestionTypeEnum.Input, TextRU = "Детали и сроки задачи?", TextAZ = "deadline?", SubCategory = subcategory });
-            var q5 = context.Questions.Add(new Question { Type = QuestionTypeEnum.Date, TextRU = "Когда приступить к задаче?", TextAZ = "Ne vaxt bashlayaq?", SubCategory = subcategory });
-            var q6 = context.Questions.Add(new Question { Type = QuestionTypeEnum.MyLocation, TextRU = "Ваш адрес", TextAZ = "Sizin adresiniz", SubCategory = subcategory });
 
-            context.Answers.Add(new Answer { TextRU = "JavaScript", TextAZ = "JavaScript", QuestionId = q3.Entity.Id });
-            context.Answers.Add(new Answer { TextRU = "Java", TextAZ = "Java", QuestionId = q3.Entity.Id });
-            context.Answers.Add(new Answer { TextRU = "Python", TextAZ = "Python", QuestionId = q3.Entity.Id });
-            context.Answers.Add(new Answer { TextRU = "PHP", TextAZ = "PHP", QuestionId = q3.Entity.Id });
-            context.Answers.Add(new Answer { TextRU = "C++", TextAZ = "C++", QuestionId = q3.Entity.Id });
-            context.Answers.Add(new Answer { TextRU = "CSS", TextAZ = "CSS", QuestionId = q3.Entity.Id });
-            context.Answers.Add(new Answer { TextRU = "C#", TextAZ = "C#", QuestionId = q3.Entity.Id });
-            context.Answers.Add(new Answer { TextRU = "C", TextAZ = "C", QuestionId = q3.Entity.Id });
-            context.Answers.Add(new Answer { TextRU = "Не знаю, нужна рекомендация специалиста", TextAZ = "Bilmirəm, mütəxəssis tövsiyəsinə ehtiyacım var", QuestionId = q3.Entity.Id });
+            #region Программисты
+            var prog = context.SubCategories.FirstOrDefault(x => x.DescriptionRU.ToLower() == "программисты");
+            var p1 = context.Questions.Add(new Question { Type = QuestionTypeEnum.Input, TextRU = "Что необходимо разработать?", TextAZ = "neynemek lazimdir?", SubCategory = prog });
+            var p2 = context.Questions.Add(new Question { Type = QuestionTypeEnum.Input, TextRU = "Для чего?", TextAZ = "Ne ucun?", SubCategory = prog });
+            var p3 = context.Questions.Add(new Question { Type = QuestionTypeEnum.Single, TextRU = "Язык программирования?", TextAZ = "Hansi dilinde?", SubCategory = prog });
+            var p4 = context.Questions.Add(new Question { Type = QuestionTypeEnum.Input, TextRU = "Детали и сроки задачи?", TextAZ = "deadline?", SubCategory = prog });
+            var p5 = context.Questions.Add(new Question { Type = QuestionTypeEnum.Date, TextRU = "Когда приступить к задаче?", TextAZ = "Ne vaxt bashlayaq?", SubCategory = prog });
+            var p6 = context.Questions.Add(new Question { Type = QuestionTypeEnum.MyLocation, TextRU = "Ваш адрес", TextAZ = "Sizin adresiniz", SubCategory = prog });
+            var p7 = context.Questions.Add(new Question { Type = QuestionTypeEnum.MeetingPoints, TextRU = "Куда вам удобно приехать?", TextAZ = "Haraya gəlmək rahatdır?", SubCategory = prog });
 
-            #region
-            //context.Answers.Add(new Answer { TextRU = "", TextAZ = "", AskedQuestion = q1.Entity, NextQuestion = q2.Entity });
+            context.SaveChanges();
 
-            //context.Answers.Add(new Answer { TextRU = "", TextAZ = "", AskedQuestion = q2.Entity, NextQuestion = q3.Entity });
+            context.Answers.Add(new Answer { TextRU = "JavaScript", TextAZ = "JavaScript", QuestionId = p3.Entity.Id });
+            context.Answers.Add(new Answer { TextRU = "Java", TextAZ = "Java", QuestionId = p3.Entity.Id });
+            context.Answers.Add(new Answer { TextRU = "Python", TextAZ = "Python", QuestionId = p3.Entity.Id });
+            context.Answers.Add(new Answer { TextRU = "PHP", TextAZ = "PHP", QuestionId = p3.Entity.Id });
+            context.Answers.Add(new Answer { TextRU = "C++", TextAZ = "C++", QuestionId = p3.Entity.Id });
+            context.Answers.Add(new Answer { TextRU = "CSS", TextAZ = "CSS", QuestionId = p3.Entity.Id });
+            context.Answers.Add(new Answer { TextRU = "C#", TextAZ = "C#", QuestionId = p3.Entity.Id });
+            context.Answers.Add(new Answer { TextRU = "C", TextAZ = "C", QuestionId = p3.Entity.Id });
+            context.Answers.Add(new Answer { TextRU = "Не знаю, нужна рекомендация специалиста", TextAZ = "Bilmirəm, mütəxəssis tövsiyəsinə ehtiyacım var", QuestionId = p3.Entity.Id });
+            #endregion
 
-            //context.Answers.Add(new Answer { TextRU = "Java", TextAZ = "Java", AskedQuestion = q3.Entity, NextQuestion = q4.Entity });
-            //context.Answers.Add(new Answer { TextRU = "JavaScript", TextAZ = "JavaScript", AskedQuestion = q3.Entity, NextQuestion = q4.Entity });
-            //context.Answers.Add(new Answer { TextRU = "C#", TextAZ = "C#", AskedQuestion = q3.Entity, NextQuestion = q4.Entity });
-            //context.Answers.Add(new Answer { TextRU = "PHP", TextAZ = "PHP", AskedQuestion = q3.Entity, NextQuestion = q4.Entity });
+            #region Переводчики
+            var translators = context.SubCategories.FirstOrDefault(x => x.DescriptionRU.ToLower() == "переводчики");
+            var t1 = context.Questions.Add(new Question { Type = QuestionTypeEnum.Multi, TextRU = "Перевод", TextAZ = "Tərcümə", SubCategory = translators });
+            var t2 = context.Questions.Add(new Question { Type = QuestionTypeEnum.Multi, TextRU = "Язык, с которого нужно перевести:", TextAZ = "Mənbə dili:", SubCategory = translators });
+            var t3 = context.Questions.Add(new Question { Type = QuestionTypeEnum.Multi, TextRU = "На какой язык перевести?", TextAZ = "Hansı dilə tərcümə edim?", SubCategory = translators });
+            var t4 = context.Questions.Add(new Question { Type = QuestionTypeEnum.Single, TextRU = "Что перевести?", TextAZ = "Nə tərcümə etmək lazımdır?", SubCategory = translators });
+            var t5 = context.Questions.Add(new Question { Type = QuestionTypeEnum.Input, TextRU = "Объём работ", TextAZ = "İşin dairəsi", SubCategory = translators });
+            var t6 = context.Questions.Add(new Question { Type = QuestionTypeEnum.Input, TextRU = "Опишите детали задачи", TextAZ = "Vəzifə detallarını təsvir edin", SubCategory = translators });
 
-            //context.Answers.Add(new Answer { TextRU = "", TextAZ = "", AskedQuestion = q4.Entity, NextQuestion = q5.Entity });
+            context.SaveChanges();
 
-            //context.Answers.Add(new Answer { TextRU = "Завтра", TextAZ = "sabax", AskedQuestion = q5.Entity, NextQuestion = q6.Entity });
-            //context.Answers.Add(new Answer { TextRU = "Послезавтра", TextAZ = "", AskedQuestion = q5.Entity, NextQuestion = q6.Entity });
-            //context.Answers.Add(new Answer { TextRU = "Пусть специалист предложит время", TextAZ = "", AskedQuestion = q5.Entity, NextQuestion = q6.Entity });
+            context.Answers.Add(new Answer { TextRU = "Устный", TextAZ = "Şifahi", QuestionId = t1.Entity.Id });
+            context.Answers.Add(new Answer { TextRU = "Письменный", TextAZ = "Yazılıb", QuestionId = t1.Entity.Id });
 
-            //context.Answers.Add(new Answer { TextRU = "15:00 — 18:00", TextAZ = "15:00 — 18:00", AskedQuestion = q6.Entity, });
-            //context.Answers.Add(new Answer { TextRU = "18:00 — 21:00", TextAZ = "18:00 — 21:00", AskedQuestion = q6.Entity,  });
-            //context.Answers.Add(new Answer { TextRU = "После 21:00", TextAZ = "После 21:00", AskedQuestion = q6.Entity,  });
+            context.Answers.Add(new Answer { TextRU = "Английский", TextAZ = "İngilis", QuestionId = t2.Entity.Id });
+            context.Answers.Add(new Answer { TextRU = "Русский", TextAZ = "Rus", QuestionId = t2.Entity.Id });
+            context.Answers.Add(new Answer { TextRU = "Азербайджанский", TextAZ = "Azərbaycan", QuestionId = t2.Entity.Id });
 
-            //context.Answers.Add(new Answer { TextRU = "", TextAZ = "", AskedQuestion = def.Entity });
+            context.Answers.Add(new Answer { TextRU = "Английский", TextAZ = "İngilis", QuestionId = t3.Entity.Id });
+            context.Answers.Add(new Answer { TextRU = "Русский", TextAZ = "Rus", QuestionId = t3.Entity.Id });
+            context.Answers.Add(new Answer { TextRU = "Азербайджанский", TextAZ = "Azərbaycan", QuestionId = t3.Entity.Id });
+
+            context.Answers.Add(new Answer { TextRU = "Техническую документацию", TextAZ = "Texniki sənədlər", QuestionId = t4.Entity.Id });
+            context.Answers.Add(new Answer { TextRU = "Художественный текст", TextAZ = "Bədii mətn", QuestionId = t4.Entity.Id });
             #endregion
 
             context.SaveChanges();
